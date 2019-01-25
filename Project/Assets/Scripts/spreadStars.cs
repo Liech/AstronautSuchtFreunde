85736712d3f23d08d98ae9cc7bf5fb990695c214
weba <weba@64d83045-0e65-4e92-91a9-31b6b1dfce6a>
2019-01-25 22:02:14 +0000

    git-svn-id: file:///C/Users/nicol/Documents/GitHub/SVN/GGJ19@28 64d83045-0e65-4e92-91a9-31b6b1dfce6a

--- Project/Assets/Scripts/spreadStars.cs
+++ Project/Assets/Scripts/spreadStars.cs
@@ -26,10 +26,10 @@ public class spreadStars : MonoBehaviour
         {
             GameObject newStar = stars[i % stars.Capacity];
             Transform t = newStar.GetComponent<Transform>();
-            t.localScale = Vector3.one * Random.Range(minScale, maxScale);
-            t.position   = new Vector3(Random.Range(-scale.x, scale.x), Random.Range(-scale.y, scale.y), 0) + pos;
+            t.localScale = Vector3.one * Random.Range(minScale, maxScale) * 0.01f;
+            t.position   = new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f), 0);
             t.rotation = Quaternion.Euler(0, 0, Random.Range(0, 360));
-            Instantiate(newStar);
+            Instantiate(newStar, this.GetComponent<Transform>() );
         }
 
     }                                                                             
