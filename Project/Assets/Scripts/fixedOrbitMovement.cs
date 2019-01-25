8fcc29a1f286e826f426318e665168f31a4eb859
weba <weba@64d83045-0e65-4e92-91a9-31b6b1dfce6a>
2019-01-25 23:58:29 +0000

    git-svn-id: file:///C/Users/nicol/Documents/GitHub/SVN/GGJ19@47 64d83045-0e65-4e92-91a9-31b6b1dfce6a

---
+++ Project/Assets/Scripts/fixedOrbitMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fixedOrbitMovement : MonoBehaviour
+{
+    [Range(0,400)]
+    public float radius = 100f;
+    [Range(-1, 1)]
+    public float orbitSpeed  = 1f;
+    [Range(-1, 1)]
+    public float rotationSpeed = 1f;
+
+    private Transform orbiterTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        orbiterTransform = GetComponent<Transform>().GetChild(0).GetComponent<Transform>();
+    }
+
+    //private void OnDrawGizmos()
+    //{
+    //    Gizmos.DrawSphere(transform.position, 3);
+    //}
+
+    // Update is called once per frame
+    void Update()
+    {
+        float t = Time.time;
+        orbiterTransform.position = transform.position + new Vector3(Mathf.Cos(t * orbitSpeed), Mathf.Sin(t * orbitSpeed), 0) * radius;
+        orbiterTransform.localRotation = Quaternion.Euler(new Vector3(0, 0, t * rotationSpeed * 360));
+    }
+}
