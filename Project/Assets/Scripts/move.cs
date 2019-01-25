821b746de0aa10c45b6ec6fbab7407fae6724fc4
weba <weba@64d83045-0e65-4e92-91a9-31b6b1dfce6a>
2019-01-25 20:41:03 +0000

    git-svn-id: file:///C/Users/nicol/Documents/GitHub/SVN/GGJ19@17 64d83045-0e65-4e92-91a9-31b6b1dfce6a

--- Project/Assets/Scripts/move.cs
+++ Project/Assets/Scripts/move.cs
@@ -16,7 +16,7 @@ public class move : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
