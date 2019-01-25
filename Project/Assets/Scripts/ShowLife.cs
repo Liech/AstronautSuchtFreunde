c8bb0c0770a7445f1dd462905b5b0149ef4fffd6
niki <niki@64d83045-0e65-4e92-91a9-31b6b1dfce6a>
2019-01-25 23:01:10 +0000

    added lifebar and life
    git-svn-id: file:///C/Users/nicol/Documents/GitHub/SVN/GGJ19@35 64d83045-0e65-4e92-91a9-31b6b1dfce6a

---
+++ Project/Assets/Scripts/ShowLife.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class ShowLife : MonoBehaviour {
+    public List<Sprite> Lifes;
+    public Life Target;
+
+	// Use this for initialization
+	void Start () {
+    Target = GameObject.Find("Player").GetComponent<Life>();
+	}
+	
+	// Update is called once per frame
+	void Update () {    
+    int Index = (int)(10.0f*((float)Target.currentLife / (float)Target.MaxLife));
+    if (Index < 0) Index = 0;    
+    GetComponent<Image>().sprite = Lifes[Index];
+ 	}
+}
