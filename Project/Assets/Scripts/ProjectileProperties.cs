78e05d045c84dc98500e867aa39c8b4e6345b738
grietha <grietha@64d83045-0e65-4e92-91a9-31b6b1dfce6a>
2019-01-25 20:04:23 +0000

    removed old stuff again
    git-svn-id: file:///C/Users/nicol/Documents/GitHub/SVN/GGJ19@12 64d83045-0e65-4e92-91a9-31b6b1dfce6a

--- Project/Assets/Scripts/ProjectileProperties.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ProjectileProperties : MonoBehaviour {
-
-    public float damage;
-    public bool fromEnemy;
-}
