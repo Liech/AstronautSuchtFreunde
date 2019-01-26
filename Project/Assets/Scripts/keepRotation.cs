0cd6b4042f5920cd59f8c48c4929bd80ccb7ab25
grietha <grietha@64d83045-0e65-4e92-91a9-31b6b1dfce6a>
2019-01-26 02:23:22 +0000

    added missing script for minimap
    git-svn-id: file:///C/Users/nicol/Documents/GitHub/SVN/GGJ19@58 64d83045-0e65-4e92-91a9-31b6b1dfce6a

---
+++ Project/Assets/Scripts/keepRotation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class keepRotation : MonoBehaviour
+{
+
+    public GameObject player = null;
+
+    private void Awake()
+    {
+        transform.position = player.transform.position;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position;
+        transform.Translate(new Vector3(0, 0, -40));
+    }
+    
+}
