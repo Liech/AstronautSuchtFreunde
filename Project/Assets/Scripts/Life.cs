6d33bcbec85eeb96a57b2be45fd2e9a5c696ad07
niki <niki@64d83045-0e65-4e92-91a9-31b6b1dfce6a>
2019-01-26 00:41:57 +0000

    the dead explodes now
    git-svn-id: file:///C/Users/nicol/Documents/GitHub/SVN/GGJ19@49 64d83045-0e65-4e92-91a9-31b6b1dfce6a

--- Project/Assets/Scripts/Life.cs
+++ Project/Assets/Scripts/Life.cs
@@ -17,6 +17,10 @@ public class Life : MonoBehaviour
   void Update()
   {
     if (currentLife <= 0)
+    {
+      GameObject g = Instantiate(Resources.Load("DeathExplosion", typeof(GameObject))) as GameObject;
+      g.transform.position = transform.position;
       Destroy(gameObject);
+    }
   }
 }
---
+++ Project/Assets/removeAfterSeconds.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class removeAfterSeconds : MonoBehaviour
+{
+  public float deleteAfterSeconds = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      StartCoroutine(rem());
+    }
+
+  IEnumerator rem()
+  {
+    yield return new WaitForSeconds(deleteAfterSeconds);
+    Destroy(gameObject);
+  }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
