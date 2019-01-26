7fd9e39e2f0d21dd3bb9479e6bf26e828b6b5668
grietha <grietha@64d83045-0e65-4e92-91a9-31b6b1dfce6a>
2019-01-26 15:48:21 +0000

    added missing script
    git-svn-id: file:///C/Users/nicol/Documents/GitHub/SVN/GGJ19@130 64d83045-0e65-4e92-91a9-31b6b1dfce6a

---
+++ Project/Assets/Scripts/HomeBase.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomeBase : MonoBehaviour
+{
+    float t;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            t = Time.time + 10f;
+            other.gameObject.GetComponent<Beam>().atHome = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            t = float.MaxValue;
+            other.gameObject.GetComponent<Beam>().atHome = false;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.name != "Player")
+            return;
+        if (Time.time > t)
+        {
+            other.gameObject.GetComponent<Life>().getDamage(-10);
+            t += 2f;
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
