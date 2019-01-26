be76a4052a806b0e00c3e9ef42f46f1f696bcd59
grietha <grietha@64d83045-0e65-4e92-91a9-31b6b1dfce6a>
2019-01-26 19:32:22 +0000

    added systems script
    git-svn-id: file:///C/Users/nicol/Documents/GitHub/SVN/GGJ19@153 64d83045-0e65-4e92-91a9-31b6b1dfce6a

---
+++ Project/Assets/Scripts/Systems.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Systems : MonoBehaviour
+{
+    public float WeaponLvL = 1;
+    public int Health = 100;
+    public float DriveFactor = 1;
+
+    int weaponLVL = 0;
+    int healthLVL = 0;
+    int driveLVL = 0;
+
+    List<float> driveFactors = new List<float> {1.0f, 1.5f, 2.5f}; 
+    List<int> healthAmounts = new List<int> {100, 150, 220}; 
+
+    public void lvlUpDrive()
+    {
+        driveLVL += 1;
+        DriveFactor = driveFactors[driveLVL];
+    }
+
+    public void lvlUpHealth()
+    {
+        healthLVL += 1;
+        Health = healthAmounts[healthLVL];
+        GetComponent<Life>().MaxLife = Health;
+        GetComponent<Life>().currentLife = Health;
+    }
+
+    public void lvlUpWeapons()
+    {
+        weaponLVL += 1;
+        WeaponLvL = weaponLVL;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
