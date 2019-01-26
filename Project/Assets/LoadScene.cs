918fa4ff11e7326280e79db6caa6408e6e539003
grietha <grietha@64d83045-0e65-4e92-91a9-31b6b1dfce6a>
2019-01-26 23:32:22 +0000

    Added Start Scene
    git-svn-id: file:///C/Users/nicol/Documents/GitHub/SVN/GGJ19@195 64d83045-0e65-4e92-91a9-31b6b1dfce6a

---
+++ Project/Assets/LoadScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+
+    public void loadMainScene()
+    {
+        SceneManager.LoadScene("Space",LoadSceneMode.Single);
+        //scen.
+    }
+
+    public void Awake()
+    {
+        GameObject.Find("EventSystem").GetComponent<EventSystem>().SetSelectedGameObject(gameObject);
+    }
+
+}
