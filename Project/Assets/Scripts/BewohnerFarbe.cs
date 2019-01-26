079f8175f69356eb55168dc3cac6f010eea66760
weba <weba@64d83045-0e65-4e92-91a9-31b6b1dfce6a>
2019-01-26 17:52:16 +0000

    git-svn-id: file:///C/Users/nicol/Documents/GitHub/SVN/GGJ19@139 64d83045-0e65-4e92-91a9-31b6b1dfce6a

---
+++ Project/Assets/Scripts/BewohnerFarbe.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BewohnerFarbe : MonoBehaviour
+{
+    public enum Farbe { grün,lila,robo,cat}
+    public Farbe farbe;
+}
--- Project/Assets/Scripts/BewohnerZuHause.cs
+++ Project/Assets/Scripts/BewohnerZuHause.cs
@@ -4,15 +4,72 @@ using UnityEngine;
 
 public class BewohnerZuHause : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject grün1;
+    public GameObject grün2;
+    public GameObject lila1;
+    public GameObject lila2;
+    public GameObject cat1 ;
+    public GameObject cat2 ;
+    public GameObject robo1;
+    public GameObject robo2;
+
+    private int Ngrün = 0;
+    private int Nlila = 0;
+    private int Ncat  = 0;
+    private int Nrobo = 0;
+
+    private float triggerT = 10f;
+
+    private void Start()
     {
-        
+        StartCoroutine(CheckBewohner());
     }
 
-    // Update is called once per frame
-    void Update()
+    private void enableHouses()
     {
-        
+        grün1.SetActive(Ngrün > 0);
+        grün2.SetActive(Ngrün > 2);
+        lila1.SetActive(Nlila > 0);
+        lila2.SetActive(Nlila > 2);
+        cat1 .SetActive(Ncat  > 0);
+        cat2 .SetActive(Ncat  > 2);
+        robo1.SetActive(Nrobo > 0);
+        robo2.SetActive(Nrobo > 2);
     }
+
+    IEnumerator CheckBewohner()
+    {
+        yield return new WaitForSeconds(triggerT);
+
+        Ngrün = 0;
+        Nlila = 0;
+        Ncat = 0;
+        Nrobo = 0;
+
+        BewohnerFarbe[] f = GetComponentsInChildren<BewohnerFarbe>();
+        for (int i = 0; i < f.Length; i++)
+        {
+            switch (f[i].farbe)
+            {
+                case BewohnerFarbe.Farbe.grün:
+                    Ngrün++;
+                    break;
+                case BewohnerFarbe.Farbe.lila:
+                    Nlila++;
+                    break;
+                case BewohnerFarbe.Farbe.robo:
+                    Nrobo++;
+                    break;
+                case BewohnerFarbe.Farbe.cat:
+                    Ncat++;
+                    break;
+                default:
+                    break;
+            }
+        }
+        enableHouses();
+
+        StartCoroutine( CheckBewohner());
+    }
+
 }
