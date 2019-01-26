176b02d0fda7f3951e60ee1ba47bacbce1ad7373
weba <weba@64d83045-0e65-4e92-91a9-31b6b1dfce6a>
2019-01-26 23:16:49 +0000

    git-svn-id: file:///C/Users/nicol/Documents/GitHub/SVN/GGJ19@192 64d83045-0e65-4e92-91a9-31b6b1dfce6a

---
+++ Project/Assets/Scripts/oscillate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class oscillate : MonoBehaviour
+{
+    public float speed = 1f;
+    private Light l;
+    // Start is called before the first frame update
+    void Start()
+    {
+        l = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        l.intensity = 10f + Mathf.Sin(Time.time * speed) * 5f;
+    }
+}
