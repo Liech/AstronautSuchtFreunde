a883ae94824bfccf1afaf75ad7835818889f2d2f
weba <weba@64d83045-0e65-4e92-91a9-31b6b1dfce6a>
2019-01-26 10:28:09 +0000

    git-svn-id: file:///C/Users/nicol/Documents/GitHub/SVN/GGJ19@80 64d83045-0e65-4e92-91a9-31b6b1dfce6a

--- Project/Assets/Scripts/fixedOrbitMovement.cs
+++ Project/Assets/Scripts/fixedOrbitMovement.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class fixedOrbitMovement : MonoBehaviour
 {
     [Range(0,400)]
     public float radius = 100f;
-    [Range(-1, 6)]
+    [Range(-6, 6)]
     public float orbitSpeed  = 1f;
     [Range(-1, 1)]
     public float rotationSpeed = 1f;
@@ -26,13 +27,12 @@ public class fixedOrbitMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-    if (orbiterTransform == null)
-    {
-      StartBossFight();
-      return;
-    }
+        if (orbiterTransform == null)
+        {
+            return;
+        }
         float t = Time.time;
-        orbiterTransform.position = transform.position + new Vector3(Mathf.Cos(t * orbitSpeed), Mathf.Sin(t * orbitSpeed), 0) * radius;
+        orbiterTransform.localPosition = new Vector3(Mathf.Cos(t * orbitSpeed), Mathf.Sin(t * orbitSpeed), 0) * radius;
         if(rotateObejct)
             orbiterTransform.localRotation = Quaternion.Euler(new Vector3(0, 0, t * rotationSpeed * 360));
     }
