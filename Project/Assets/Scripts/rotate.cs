9a73553189fced32b0c19b4488ccc26a8d2c314a
weba <weba@64d83045-0e65-4e92-91a9-31b6b1dfce6a>
2019-01-26 21:19:57 +0000

    git-svn-id: file:///C/Users/nicol/Documents/GitHub/SVN/GGJ19@172 64d83045-0e65-4e92-91a9-31b6b1dfce6a

---
+++ Project/Assets/Scripts/rotate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+    public float rotationSpeed = 1f;
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.localRotation = Quaternion.Euler(new Vector3(0, 0, Time.time * rotationSpeed));
+    }
+}
