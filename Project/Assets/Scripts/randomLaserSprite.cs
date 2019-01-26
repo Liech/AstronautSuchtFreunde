f8ab85ed9fc97d761593c9ca7ca46bba739c2af1
weba <weba@64d83045-0e65-4e92-91a9-31b6b1dfce6a>
2019-01-26 16:57:54 +0000

    git-svn-id: file:///C/Users/nicol/Documents/GitHub/SVN/GGJ19@136 64d83045-0e65-4e92-91a9-31b6b1dfce6a

---
+++ Project/Assets/Scripts/randomLaserSprite.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomLaserSprite : MonoBehaviour
+{
+    public List<Sprite> sprites;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<SpriteRenderer>().sprite = sprites[Random.Range(0, sprites.Count - 1)];
+        Vector3 dir = transform.parent.GetComponent<Rigidbody2D>().velocity;
+        transform.rotation = Quaternion.FromToRotation(Vector3.up, dir);
+    }
+}
