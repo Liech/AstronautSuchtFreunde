9c0007319b68a823efb776e6034821b7cf747142
chris <chris@64d83045-0e65-4e92-91a9-31b6b1dfce6a>
2019-01-26 20:15:19 +0000

    git-svn-id: file:///C/Users/nicol/Documents/GitHub/SVN/GGJ19@163 64d83045-0e65-4e92-91a9-31b6b1dfce6a

---
+++ Project/Assets/Scripts/ShowDudes.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowDudes : MonoBehaviour
+{
+    public List<Sprite> Lifes;
+    public Life Target;
+
+    public List<GameObject> dudes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Target = GameObject.Find("Player").GetComponent<Life>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var beam = GameObject.Find("Player").GetComponent<Beam>();
+
+        for (int i = 0; i < dudes.Count; i++)
+        {
+            GameObject newDude = Instantiate(dudes[i % dudes.Count], this.GetComponent<Transform>());
+            //Transform t = newAsteroid.GetComponent<Transform>();
+            //t.localScale = Vector3.one * Random.Range(minScale, maxScale);
+
+
+            newDude.GetComponent<RectTransform>().Translate(new Vector3(42f*i, 0,0));
+            //newAsteroid.GetComponent<AsteroidAsset>().screenSize = screenSize;
+            //newAsteroid.GetComponent<AsteroidAsset>().velocityScaling = velocityScaling;
+            newDude.GetComponent<Image>().sprite = dudes[i].GetComponent<SpriteRenderer>().sprite;
+            //Debug.Log(dude.name.ToString());
+            //Lifes.Add(dude.GetComponent<SpriteRenderer>().sprite);
+        }
+
+        if ( Lifes.Count > 0 )
+          GetComponent<Image>().sprite = Lifes[0];
+    }
+}
---
+++ Project/Assets/Scripts/showDudesInShip.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class showDudesInShip : MonoBehaviour
+{
+    public Life Target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Target = GameObject.Find("Player").GetComponent<Life>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var beam = Target.GetComponent<Beam>();
+        for ( int i = 0; i < beam.storedDudes.Count; ++i)
+        {
+            if (i >= 5) continue;
+            name = "DudeInShip_" + (i + 1).ToString();
+            gameObject.transform.Find(name).GetComponent<Image>().sprite = beam.storedDudes[i].GetComponent<SpriteRenderer>().sprite;
+            gameObject.transform.Find(name).GetComponent<Image>().color = Color.white;
+        }
+
+        for (int i = beam.storedDudes.Count; i < 5;  ++i)
+        {
+            name = "DudeInShip_" + (i + 1).ToString();
+            gameObject.transform.Find(name).GetComponent<Image>().color = Color.clear;
+        }
+
+    }
+}
