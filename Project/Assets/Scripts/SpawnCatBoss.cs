b577162b2816e26a145bb16fc2ea9c9992a59a9a
niki <niki@64d83045-0e65-4e92-91a9-31b6b1dfce6a>
2019-01-26 19:10:10 +0000

    boss spawn mechanic added
    git-svn-id: file:///C/Users/nicol/Documents/GitHub/SVN/GGJ19@147 64d83045-0e65-4e92-91a9-31b6b1dfce6a

---
+++ Project/Assets/Scripts/SpawnCatBoss.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(gravityInfluence))]
+public class SpawnCatBoss : MonoBehaviour
+{
+  public GameObject boss;
+    public float radius;
+  public bool showradius = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    Collider2D[] col = Physics2D.OverlapCircleAll(transform.position, radius);
+    for(int i = 0;i < col.Length;i++)
+    {
+      if (col[i].gameObject.GetComponent<CatShip>() != null)
+        return;
+    }
+    GameObject g = Instantiate(boss);
+    g.transform.position = transform.position;
+    g.transform.GetChild(0).GetComponent<rotationobject>().Target = gameObject;
+    Destroy(this);
+    }
+
+  public void OnDrawGizmos()
+  {
+    if (showradius)
+    Gizmos.DrawSphere(transform.position, radius);
+  }
+}
