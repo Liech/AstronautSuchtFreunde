f949d6ebee40487b99c5422d3d834d2d4d19959b
grietha <grietha@64d83045-0e65-4e92-91a9-31b6b1dfce6a>
2019-01-26 10:09:22 +0000

    added missing scripts
    git-svn-id: file:///C/Users/nicol/Documents/GitHub/SVN/GGJ19@76 64d83045-0e65-4e92-91a9-31b6b1dfce6a

---
+++ Project/Assets/Scripts/GravityField.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityField : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public Vector2 getGravity(Vector3 pos)
+    {
+        var fields = GetComponentsInChildren<gravityInfluence>();
+        var grav = Vector2.zero;
+        foreach(var field in fields)
+        {
+            grav += field.getForce(pos);
+        }
+        return grav;
+    }
+}
