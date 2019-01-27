31013429fd6f7f7209339132609d21acf3d23f74
chris <chris@64d83045-0e65-4e92-91a9-31b6b1dfce6a>
2019-01-27 00:46:52 +0000

    added weapon level icon
    git-svn-id: file:///C/Users/nicol/Documents/GitHub/SVN/GGJ19@207 64d83045-0e65-4e92-91a9-31b6b1dfce6a

---
+++ Project/Assets/Scripts/ShowDrive.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class ShowDrive : MonoBehaviour {
+    public List<Sprite> Lifes;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int level = GameObject.Find("Player").GetComponent<Systems>().driveLVL;
+        GetComponent<Image>().sprite = Lifes[level];
+ 	}
+}
