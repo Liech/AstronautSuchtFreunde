5cb1b1b711aab8541ea9f513e14d02c2d776330d
weba <weba@64d83045-0e65-4e92-91a9-31b6b1dfce6a>
2019-01-27 10:55:15 +0000

    git-svn-id: file:///C/Users/nicol/Documents/GitHub/SVN/GGJ19@224 64d83045-0e65-4e92-91a9-31b6b1dfce6a

---
+++ Project/Assets/Scripts/setZzero.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class setZzero : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 pos = transform.position;
+        pos.z = 0f;
+        transform.position = pos;
+    }
+
+}
