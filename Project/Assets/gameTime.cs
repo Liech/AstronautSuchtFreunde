786d74fcda5a9dc660a199b606220ea5e6e44e04
chris <chris@64d83045-0e65-4e92-91a9-31b6b1dfce6a>
2019-01-27 12:33:10 +0000

    added game time
    git-svn-id: file:///C/Users/nicol/Documents/GitHub/SVN/GGJ19@244 64d83045-0e65-4e92-91a9-31b6b1dfce6a

---
+++ Project/Assets/gameTime.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gameTime : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int duration = (int)(Time.time - Systems.start_time);
+        string str = (duration / 60).ToString("D2") + ":" + (duration % 60).ToString("D2");
+        GetComponent<Text>().text = str;
+    }
+}
