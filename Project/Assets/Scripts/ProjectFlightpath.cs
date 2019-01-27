1f31a6304ef9875199c32d1f7447fd997f27057e
weba <weba@64d83045-0e65-4e92-91a9-31b6b1dfce6a>
2019-01-27 09:46:32 +0000

    git-svn-id: file:///C/Users/nicol/Documents/GitHub/SVN/GGJ19@216 64d83045-0e65-4e92-91a9-31b6b1dfce6a

--- Project/Assets/Scripts/ProjectFlightpath.cs
+++ Project/Assets/Scripts/ProjectFlightpath.cs
@@ -10,19 +10,23 @@ public class ProjectFlightpath : MonoBehaviour
     [Range(10, 200)]
     public int numberOfSteps = 200;
     LineRenderer lr;
+    LineRenderer lr_miniMap;
     public GravityField gField;
     // Start is called before the first frame update
     void Start()
     {
         lr = GetComponent<LineRenderer>();
+        lr_miniMap = transform.GetChild(0).GetComponent<LineRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         lr.positionCount = numberOfSteps;
+        lr_miniMap.positionCount = numberOfSteps;
         var pos = transform.position;
         lr.SetPosition(0, transform.position);
+        lr_miniMap.SetPosition(0, transform.position);
         var velo = GetComponentInParent<Rigidbody2D>().velocity;
         for (int i = 1; i < numberOfSteps; i++)
         {
@@ -31,6 +35,7 @@ public class ProjectFlightpath : MonoBehaviour
             //var mov = (velo + gForce);
             pos += new Vector3(stepsize*velo.x,stepsize*velo.y, 0); 
             lr.SetPosition(i, pos);
+            lr_miniMap.SetPosition(i, pos);
         }
     }
 }
