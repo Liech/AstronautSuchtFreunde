c99e15096593e1f372d7dba52bea132db3b0933e
Nicolas Harttig <nicolas_harttig@hotmail.de>
2022-07-10 11:53:20 +0200

    Introduced Damage Lock to prohibit Death of natural Causes

--- Project/Assets/Scripts/CollisionDamage.cs
+++ Project/Assets/Scripts/CollisionDamage.cs
@@ -10,6 +10,7 @@ public class CollisionDamage : MonoBehaviour
     List<float> damageFactors = new List<float> { 1.0f, 1.3f, 1.8f, 2.5f };
     GameObject player;
     public bool shotByPlayer = false;
+    public bool DamageLock = false; // Prohibit Random Deaths without Player interaction
 
     // Start is called before the first frame update
     void Start()
@@ -30,14 +31,17 @@ public class CollisionDamage : MonoBehaviour
 
         CollisionDamage otherCD = collision.gameObject.GetComponent<CollisionDamage>();
         if (otherCD)
-            if (otherCD.shotByPlayer)
+            if (otherCD.shotByPlayer) {
                 factor = damageFactors[player.GetComponent<Systems>().WeaponLvL - 1];
+                DamageLock = false;
+            }
 
         //Debug.Log("WeaponLevel: " + GameObject.Find("Player").GetComponent<Systems>().WeaponLvL.ToString() + ", factor: " + factor.ToString());
         float diff = collision.relativeVelocity.magnitude;
         if (diff > 20f)
             diff = 20f;
-
+        if (DamageLock)
+            return;
         GetComponent<Life>().getDamage((int)(diff * factor));
     }
 }
